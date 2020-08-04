1310a8793c6ada1b3a18538a59f4e2a4d8a98e89
Arhua <a0987837233@gmail.com>
2020-08-04 12:21:55 +0800

    首頁影片

--- src/Pages/MainPages/Home/Home.js
+++ src/Pages/MainPages/Home/Home.js
@@ -8,6 +8,7 @@ import { ReactComponent as LogoFail } from '../../../Assets/img/fail.svg'
 import { ReactComponent as LogoNew } from '../../../Assets/img/new.svg'
 import { ReactComponent as LogoPercentage } from '../../../Assets/img/percentage.svg'
 import { ReactComponent as LogoTotal } from '../../../Assets/img/total.svg'
+import { EasyButton } from '../../../Components/Buttons';
 
 export const Home = (props) => {
 
@@ -20,15 +21,39 @@ export const Home = (props) => {
         <>
             {/* 寬度大於等於768時渲染的組件 */}
             {width > 768 && <BasicContainer theme={home.basicContainer}>
+                <video width="100%" height="644" autoplay="autoplay" muted="muted">
+                    <source src={"/45b0847.mp4"} type="video/mp4" />
+                </video>
+                <EasyButton
+                    text={"預約足測"}
+                    onClick={() => { history.push("/Reservation") }}
+                    theme={{
+                        backgroundColor: "#964f19",
+                        color: "#fff",
+                        borderRadius: "4px",
+                        textAlign: "center",
+                        fontSize: "14px",
+                        cursor: "pointer",
+                        fontWeight: 400,
+                        width: "166px",
+                        height: "40px",
+                        lineHeight: "40px",
+                        margin: "auto auto 12px",
+                        hoverBackgroundColor: "#6d3f00",
+                    }}></EasyButton>
                 <div style={{ height: "1000rem" }}>
                     sdfsdf
                 </div>
                 大於等於768時渲染的組件
+
             </BasicContainer>
             }
 
             {/* 寬度小於768時渲染的組件 */}
             {width <= 768 && <BasicContainer theme={home.basicContainer}>
+                <video width="100%" height="644" autoplay="autoplay" muted="muted">
+                    <source src={"/45b0847.mp4"} type="video/mp4" />
+                </video>
                 <div style={{ height: "1000rem" }}>
                     sdfsdf
                 </div>
