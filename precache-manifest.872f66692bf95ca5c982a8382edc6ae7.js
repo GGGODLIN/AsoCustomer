c7283a39cf9cfa1e3d39ba454c584afcde8a0545
GGGODLIN <qwe70301@gmail.com>
2020-08-12 18:31:15 +0800

    Updates

---
+++ precache-manifest.872f66692bf95ca5c982a8382edc6ae7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dbbe486fce9e9323e3a35c62d9d9930",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7c73a732477591d9061",
+    "url": "/static/css/2.5a5c6f85.chunk.css"
+  },
+  {
+    "revision": "309c49fe02a144aab974",
+    "url": "/static/css/main.66810dcb.chunk.css"
+  },
+  {
+    "revision": "c7c73a732477591d9061",
+    "url": "/static/js/2.131a6a6a.chunk.js"
+  },
+  {
+    "revision": "27fdc7e46dd7e5eb85baa6e45983f496",
+    "url": "/static/js/2.131a6a6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "309c49fe02a144aab974",
+    "url": "/static/js/main.7e93d61f.chunk.js"
+  },
+  {
+    "revision": "6a59bd51eb184ae677f3",
+    "url": "/static/js/runtime-main.836b504c.js"
+  },
+  {
+    "revision": "ba00bf16beccf1d6e7a0a2c909f1722f",
+    "url": "/static/media/done.ba00bf16.svg"
+  },
+  {
+    "revision": "056b5c086476a08e69a93140a7a196a1",
+    "url": "/static/media/down.056b5c08.svg"
+  },
+  {
+    "revision": "b02bdc1b846fd65473922f5f62832108",
+    "url": "/static/media/element-icons.b02bdc1b.ttf"
+  },
+  {
+    "revision": "d2f69a92faa6fe990d2e613c358be705",
+    "url": "/static/media/element-icons.d2f69a92.woff"
+  },
+  {
+    "revision": "30f9ec3ee6d30425a02992540bb7bb82",
+    "url": "/static/media/fail.30f9ec3e.svg"
+  },
+  {
+    "revision": "2e85d96b9b5b86b32e06e9cbe93748a2",
+    "url": "/static/media/footQAndA.2e85d96b.svg"
+  },
+  {
+    "revision": "94bfb3935caa66897c583cacb3e99512",
+    "url": "/static/media/new.94bfb393.svg"
+  },
+  {
+    "revision": "732f5424d027f7c3aa8a00c714fb3aea",
+    "url": "/static/media/percentage.732f5424.svg"
+  },
+  {
+    "revision": "810220c21aecfe57369bf5eb45418320",
+    "url": "/static/media/total.810220c2.svg"
+  }
+]);
